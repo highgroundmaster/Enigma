ebaf455096ae0963de9284d64d0340927bd18929
Jyotika <jyotikam@iitbhilai.ac.in>
2020-09-28 23:22:24 +0530

    addition in Bot.py

--- Bot.py
+++ Bot.py
@@ -5,6 +5,14 @@ client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
-    print('Bot is ready.')
+    print('APL is ready to work.')
+
+@client.event
+async def on_member_join(member):
+    print(f'Yayy {member} has joined the server')
+
+@client.event
+async def on_member_remove(member):
+    print(f'Goodbye {member}')
 
 client.run('NzYwMDU2NDE2OTQ5MzcwOTA5.X3Gfsg.lGMgtSiIzO_XSSTp-AVV5qe6y_A')
