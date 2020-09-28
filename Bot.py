21f15b47eb131bb5535d6f9e127889482229428e
Jyotika <jyotikam@iitbhilai.ac.in>
2020-09-29 00:23:15 +0530

    modified Bot.py

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -14,5 +15,24 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print(f'Goodbye {member}')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[-3]}')
+
+
 
 client.run('NzYwMDU2NDE2OTQ5MzcwOTA5.X3Gfsg.lGMgtSiIzO_XSSTp-AVV5qe6y_A')
